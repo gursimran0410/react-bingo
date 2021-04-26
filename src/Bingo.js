6ad942985dfd05e30d029fd3f05cb3be0f8c4e66
Gursimran Singh <gursimransingh154@gmail.com>
2021-04-26 20:48:28 +0530

    Added Reset and Shuffle Bingo Button

--- src/Bingo.js
+++ src/Bingo.js
@@ -6,15 +6,7 @@ var wonSeries = [[], [], [], []];
 
 export default function Bingo() {
   const [state, setState] = useState({ checked: { 12: true } });
-
-  function Confetti() {
-    useEffect(() => {
-      start();
-    });
-    return <canvas id="canvas" />;
-  }
-
-  var bingoArray = [
+  const [bingoArray, setArray] = useState([
     { id: 0, value: "child noises in the background" },
     { id: 1, value: "Hello, hello?" },
     { id: 2, value: "I need to jump in another call" },
@@ -40,7 +32,42 @@ export default function Bingo() {
     { id: 22, value: "you will send the minutes" },
     { id: 23, value: "sorry I was on mute" },
     { id: 24, value: "can you repeat please" }
-  ];
+  ]);
+
+  function Confetti() {
+    useEffect(() => {
+      start();
+    });
+    return <canvas id="canvas" />;
+  }
+
+  // var bingoArray = [
+  //   { id: 0, value: "child noises in the background" },
+  //   { id: 1, value: "Hello, hello?" },
+  //   { id: 2, value: "I need to jump in another call" },
+  //   { id: 3, value: "Can Everyone Go On Mute" },
+  //   { id: 4, value: "Could you please get closer to the mic" },
+  //   { id: 5, value: "Load Painful Echo/ Feedback" },
+  //   { id: 6, value: "Next Slide Please" },
+  //   { id: 7, value: "can we take this offline" },
+  //   { id: 8, value: "is __ on call" },
+  //   { id: 9, value: "Could You Share This Slide Afterwards" },
+  //   { id: 10, value: "can somebody grant presenter rights" },
+  //   { id: 11, value: "can you email that to everyone" },
+  //   { id: 12, value: "Conf Call Bingo" },
+  //   { id: 13, value: "Sorry I had problems logging in" },
+  //   { id: 14, value: "animal noises in the background" },
+  //   { id: 15, value: "sorry I didnt found the conference id" },
+  //   { id: 16, value: "I was having connection issues" },
+  //   { id: 17, value: "I will have to get back to you" },
+  //   { id: 18, value: "who just joined?" },
+  //   { id: 19, value: "I cannot hear anyone" },
+  //   { id: 20, value: "do you see any screen" },
+  //   { id: 21, value: "let us wait for other person" },
+  //   { id: 22, value: "you will send the minutes" },
+  //   { id: 23, value: "sorry I was on mute" },
+  //   { id: 24, value: "can you repeat please" }
+  // ];
 
   const isWon = (checked) => {
     const range = [0, 1, 2, 3, 4];
@@ -170,16 +197,36 @@ export default function Bingo() {
     }, 5000);
   };
 
+  const shuffleArray = () => {
+    setArray(bingoArray.sort(() => Math.random() - 0.5));
+    setState({ checked: { 12: true } });
+    for (var i = 0; i < bingoArray.length; i++) {
+      if (bingoArray[i].id === 12) {
+        for (var j = 0; j < bingoArray.length; j++) {
+          if (j === 12) {
+            let temp = bingoArray[i];
+            bingoArray[i] = bingoArray[j];
+            bingoArray[j] = temp;
+          }
+        }
+      }
+    }
+  };
+
   const handleOnclick = (id) => {
-    setState((state) => {
-      const checked = { ...state.checked, [id]: !state.checked[id] };
-      const won = isWon(checked);
-      return {
-        ...state,
-        checked,
-        won
-      };
-    });
+    if (id === 12) {
+      shuffleArray();
+    } else {
+      setState((state) => {
+        const checked = { ...state.checked, [id]: !state.checked[id] };
+        const won = isWon(checked);
+        return {
+          ...state,
+          checked,
+          won
+        };
+      });
+    }
   };
 
   const checkIfStillWon = () => {
